cf315db1bb18dd086dfa1725c09845e0a10bf809
Айдильдин Нурболат <Nurbolat@MBP-mac-4.fritz.box>
2026-03-13 12:12:25 +0100

    commit

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+export const supabaseUrl = 'https://alvzljnhuaesrkrpukdn.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsdnpsam5odWFlc3JrcnB1a2RuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzc5NDIsImV4cCI6MjA4ODkxMzk0Mn0.qW2jSpM3JMyvTnB2dZqFUui0QuVbG92Lv7y_U4PA9xI'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase
